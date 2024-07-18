7d71ef9a23af193334d381ce43bc31406c82823c
Mercy Uzor <>
2024-07-18 08:26:44 +0100

    fix update the env for firebase

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -1,24 +1,11 @@
 import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
-
-const firebaseServiceKeys = {
-  type: process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_TYPE,
-  project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
-  client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
-  auth_uri: process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI,
-  token_uri: process.env.NEXT_PUBLIC_FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_AUTH_PROVIDERS,
-  client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.NEXT_PUBLIC_FIREBASE_UNIVERSAL_DOMAIN,
-};
-const serviceAccount = JSON.stringify(firebaseServiceKeys);
+const firebaseAct = process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY;
+const serviceAccount = JSON.parse(firebaseAct as string);
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(serviceAccount)),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
