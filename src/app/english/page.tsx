706adfc5da5fde5989e64603d68f7c184d84f533
Mercy Uzor <>
2024-07-18 05:22:52 +0100

    updated empty page for tables

--- src/app/english/page.tsx
+++ src/app/english/page.tsx
@@ -1,1 +1,9 @@
+const Page = () => {
+  return (
+    <>
+      {/* TODO: this should be the table having all the english word so it can be managed properly */}
+    </>
+  );
+};
 
+export default Page;
---
+++ src/app/language/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <>
+      {/* TODO: this should be the table having all the language word so it can be managed properly */}
+    </>
+  );
+};
+
+export default Page;
