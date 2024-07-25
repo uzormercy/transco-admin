0f9c2824ec7f811e9ddf32ee5aa15017ca7d6719
Mercy Uzor <>
2024-07-25 14:12:07 +0100

    fix

--- src/services.ts
+++ src/services.ts
@@ -10,3 +10,13 @@ export const getLanguages = async () => {
   });
   return collections;
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDGf18vN59kQKxqVSknYXS2vepizNa9Llg",
+//   authDomain: "langpro-d2453.firebaseapp.com",
+//   projectId: "langpro-d2453",
+//   storageBucket: "langpro-d2453.appspot.com",
+//   messagingSenderId: "829791214713",
+//   appId: "1:829791214713:web:6d1191df6698006fc848a8",
+//   measurementId: "G-2QH3W4YYLY"
+// };
